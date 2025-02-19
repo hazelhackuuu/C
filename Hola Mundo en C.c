8ed12e56acf5c6f5ac078af20776456261c512fc
Hazel <sotovargasdiegoo@gmail.com>
2025-02-19 15:59:56 -0600

    Hola Mundo en C.c

---
+++ Hola Mundo en C.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  //<-- Incluimos la biblioteca.
+ 
+
+int main(){   // <-- Leemos la biblioteca y le adjuntamos corchetes para dar inicio y fin.
+
+    printf("Hola Mundo, este es mi primer mensaje !!!");  //<-- Se lee el mensaje yse termina con comillas;.
+
+return 0;
+}
