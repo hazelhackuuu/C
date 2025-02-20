4d27824367f269a592db7227ec7323b9e97d5e1d
Hazel <sotovargasdiegoo@gmail.com>
2025-02-20 15:25:59 -0600

    Tipos de datos (char).c

---
+++ Tipos de datos (char).c
@@ -0,0 +1,20 @@
+                          //Tipos de Datos
+
+//EJEMPLO NUMERO 1
+
+#include <stdio.h>
+
+int main() {  
+
+ char Libro []= "CyberCEO";  
+ //Para poder imprimir se pone char seguido de la variable y [] y el dato de la variable en comillas dobles.
+
+  printf("%s\n", Libro);
+//Para poder imprimir se acompaña de printf y "%\n" todos esto para poder señalar el dato, seguido de la variable.
+ 
+ char Libro1 []= "Asi es como me dicen qué terminara el mundo";
+  printf("%s\n", Libro1);
+
+ 
+return 0 ;
+}
