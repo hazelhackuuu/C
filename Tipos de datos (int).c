73d7f39f49a28c98bc92f2030fe82c321fb9e4bd
Hazel <sotovargasdiegoo@gmail.com>
2025-02-21 22:16:24 -0600

    Tipos de datos (int).c

---
+++ Tipos de datos (int).c
@@ -0,0 +1,15 @@
+///TIPOS DE DATOS
+
+//EJEMPLO NUMERO 1
+
+#include <stdio.h>
+
+int main(){
+
+float decimal = 1.398945774;  //Para poder imprimir el siguiente se le agrega %f comos signo de qué es un valor flotante y junto con \n para indicar qué es una linea nueva.
+printf("%f\n", decimal);
+
+float decimal1 = 1.784593883;
+printf("%f\n", decimal1);
+
+ return 0;
