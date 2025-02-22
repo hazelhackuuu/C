5f4c14981b349bad9c4fdbbc18469d35a0cc4302
Hazel <sotovargasdiegoo@gmail.com>
2025-02-21 22:17:33 -0600

    Tipos de datos (int).c

---
+++ Tipos de datos (int).c
@@ -0,0 +1,19 @@
+                   //TIPOS DE DATOS (char )
+
+
+//EJEMPLO NUMERO 1
+
+#include <stdio.h> 
+
+int main(){
+
+int numero1= 100;   //Declaramos la variable como entero y le adjuntamos %d para poder imprimirlo junto con \n para una linea nueva.
+printf("%d\n", numero1); 
+
+
+int numero2 = 200;   
+printf("%d\n", numero2);
+
+
+return 0;
+}
