4e50826f29b3d221cb968ca9ca007cb4799a5f56
Hazel <sotovargasdiegoo@gmail.com>
2025-02-25 00:06:11 -0600

    Listas de texto en .c

---
+++ Listas de texto en .c
@@ -0,0 +1,21 @@
+//CREAR LISTAS EN C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+//Definimos la variable junto con los datos.
+
+//                   0        1       2        3          4
+char *animales[]= {"loro", "perro", "gato","serpiente", "conejo"};
+
+printf("%s\n", animales[4]);
+
+//Para poder imprimir se usa "%s\n y el nombre de la variable y para seleccionar se usa[]"
+
+
+
+
+return 0;
+}
