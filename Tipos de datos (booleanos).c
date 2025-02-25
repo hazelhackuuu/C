e61d2157a815ee7400aed3bba455ec44551a9fb5
Hazel <sotovargasdiegoo@gmail.com>
2025-02-24 23:57:33 -0600

    Tipos de datos (booleanos).c

---
+++ Tipos de datos (booleanos).c
@@ -0,0 +1,26 @@
+///TIPOS DE DATOS (BOOLEANOS)
+
+//LOS DATOS BOOLEANOS, NO EXISTEN EN C PERO ESTOS SE PUEDEN REPRESENTAR COMO 0 Y 1 DONDE 0 ES FALSE Y 1 ES TRUE.
+
+
+//EJEMPLO NUMERO 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+//               0  1  2  3  4  5  6
+int numeros[] = {1, 2, 3, 4, 5, 6, 7,};
+printf("%d\n", numeros[1]);
+
+
+
+
+
+
+
+
+return 0;
+}
+
