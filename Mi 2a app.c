600b7d34ddb1086c03fe60d812fe697b2acba62d
Hazel <sotovargasdiegoo@gmail.com>
2025-04-08 18:14:41 -0600

    Mi 2a app.c

---
+++ Mi 2a app.c
@@ -0,0 +1,105 @@
+                               //ACTIVIDAD NUMERO 2 "MI SEGUNDA APP"
+
+
+
+//Nombre de la materia: Programacion I
+
+//Nombre del Alumno: Hazel.
+
+//Nombre de la Carrera: Ingenieria en Ciberseguridad.
+
+//Nombre del Profesor: Alcides Arbona.
+
+//Fecha: 28/02/2025
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+
+int main()
+{
+
+                                                     //CREACION DE CONSTANTES Y VARIABLES
+
+ const int diasdelasemana = 7;
+
+ const float PI = 3.1416;
+             
+ const float IVA = 0.18;                       /// <----- CONSTANTES Y VARIABLES
+
+ const float IVAA = 0.20;
+
+ const int diasdelaño = 365;
+
+
+
+
+
+
+ printf("La semana tiene un total de: %d dias \n", diasdelasemana);
+
+
+ printf("El area de un circulo con un radio de 3cm es: %f\n", PI*3*3);
+ 
+
+ printf("El año tiene un total de: %d dias \n"+ diasdelaño);
+
+
+
+
+
+                                                      
+                                                 //PEQUEÑA CALCULADORA EN C 
+
+ float valor1 = 20;
+
+ float valor2 = 30;                                                       // <--- OPERADOR DE SUMA 
+                                                
+ printf("El resultado de la suma es: %f\n", valor1 + valor2);
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+float valor3 = 60;
+
+float valor4 = 20;                                                       // <-- OPERADOR DE RESTA
+
+printf("El resultado de la resta es: %f\n", valor3 - valor4);
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+float valor5 = 50;
+
+float valor6 = 40;
+
+printf("El resultado de la multiplicacion es:%f\n", valor5 *valor6);   // <-- OPERADOR DE MULTIPLICACION
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+ float valor7 = 200;
+
+ float valor8 = 50;
+
+ printf("El resultado de la division es: %f\n", valor7/valor8);       // <-- OPERADOR DE DIVISION
+
+
+
+
+    return 0;
+}
