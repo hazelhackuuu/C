91b254a67028b993bf463d20329aec4255b4317f
Hazel <sotovargasdiegoo@gmail.com>
2025-04-08 18:24:02 -0600

    Operadores_en_c.c

---
+++ Operadores_en_c.c
@@ -0,0 +1,60 @@
+                                     //OPERADORE EN C 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+//SUMA
+
+
+int numero = 20;
+
+int numero2 = 30;
+
+
+printf("El valor de los 2 numeros es;%d\n", numero + numero2);
+
+
+
+
+
+//RESTA
+
+
+
+int numero3 = 40;
+
+int numero4 = 60;
+
+printf("El resultado de la resta de los 2 numeros es;%d\n", numero3 + numero4);
+
+
+
+//MULTIPLICACION
+
+
+int numero5 = 70;
+
+int numero6 = 80;
+
+
+printf("El resultado de la resta de los 2 numeros es%d\n", numero5 - numero6);
+
+
+
+
+//MULTIPLICACION
+
+int numero7 =  90;
+
+int numero8= 100;
+
+printf("El resultado de la multiplicacion es:%d\n", numero7%numero8);
+
+
+
+
+return 0;
+}
